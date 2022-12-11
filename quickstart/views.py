f6ad6e9b49490ae3501c72d86c837b001a80ae8f
duvaliertsagmo <edtsagmo01@monccnb.ca>
2022-12-11 17:25:31 -0400

    afficher les donnees a la page html

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http.response import JsonResponse
 from rest_framework import viewsets
 from projetfinal.serializers import MicrocontroleurSerializer
 from quickstart.models import Microcontroleur
@@ -22,10 +23,18 @@ def Micro(request):
       microcontroleur.modele = Valeur
       microcontroleur.serie = Date
       microcontroleur.save()
-      
+      #enregistre la derniere donnes capteer
+      Microcontroleur.objects.all().delete()
       return HttpResponse('<h1>Success</h1>')   
     if request.method == 'GET':
-        return HttpResponse('<h1>Get request</h1>')  
+        is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+        if is_ajax:
+            todos = list(Microcontroleur.objects.all().values())
+            return JsonResponse({'context': todos})
+
+        #data = Microcontroleur.objects.all()
+        #dataJson = MicrocontroleurSerializer(data, many = True)
+        #return JsonResponse(dataJson.data, safe=False)  
 
 class MicrocontroleurViewSet(viewsets.ModelViewSet):
     """
